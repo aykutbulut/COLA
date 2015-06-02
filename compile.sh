9897c9514d69aa39d4597ab74678ee96cbceec6d
Aykut Bulut <aykutblt@gmail.com>
2015-06-02 14:31:44 -0400

    get_dependencies.sh and compile.sh scripts added.

---
+++ compile.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+mkdir build
+cd build
+build_dir=$PWD
+inc_dir=${build_dir%%/}/include
+lib_dir=${build_dir%%/}/lib
+pkg_dir=${lib_dir%%/}/pkgconfig
+PKG_CONFIG_PATH=${pkg_dir}:$PKG_CONFIG_PATH
+export CXXFLAGS=-std=c++11
+# configure and install CoinUtils
+mkdir CoinUtils
+cd CoinUtils
+../../CoinUtils/configure --prefix=$build_dir
+make -j 10 install
+cd ..
+# configure and install Osi
+mkdir Osi
+cd Osi
+../../Osi/configure --prefix=$build_dir
+make -j 10 install
+cd ..
+# configure and install Clp
+mkdir Clp
+cd Clp
+../../Clp/configure --prefix=$build_dir
+make -j 10 install
+cd ..
+#configure and install OsiConic
+mkdir OsiConic
+cd OsiConic
+../../OsiConic/configure --prefix=$build_dir
+make -j 10 install
+cd ..
+#configure and install Cola
+mkdir Cola
+cd Cola
+../../configure --prefix=$build_dir
+make -j 10 install
+cd ..
---
+++ get_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# get blas
+svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Blas/stable/1.4 ThirdParty/Blas
+# get lapack
+svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Lapack/stable/1.5 ThirdParty/Lapack
+# get coin-or coinutils
+svn co https://projects.coin-or.org/svn/CoinUtils/stable/2.10/CoinUtils
+# get coin-or osi
+svn co https://projects.coin-or.org/svn/Osi/stable/0.107/Osi
+# get coin-or clp
+svn co https://projects.coin-or.org/svn/Clp/stable/1.16/Clp
+# get osiconic
+git clone https://github.com/aykutbulut/OSI-CONIC OsiConic
