6ee662a4b58c5f27e886ac087ac30c303fb1f4d0
Aykut Bulut <aykutblt@gmail.com>
2015-06-09 15:30:19 -0400

    simple cola solver example added.

---
+++ examples/solver/solver.cpp
@@ -0,0 +1,13 @@
+#include <ColaModel.hpp>
+
+int main(int argc, char ** argv) {
+  ColaModel * m = new ColaModel();
+  m->readMps(argv[1]);
+  m->initialSolve();
+  std::cout << "Optimal Solution is " << m->getObjValue()
+            << std::endl;
+  std::cout << "Number of iterations is " << m->getIterationCount()
+            << std::endl;
+  delete m;
+  return 0;
+}
