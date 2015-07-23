c45d8464c96ee5ce06ce227be99ccf2a3ada00d7
Aykut Bulut <aykutblt@gmail.com>
2015-07-23 12:20:04 -0400

    compile and dependency scripts updated.

--- compile.sh
+++ compile.sh
@@ -6,18 +6,18 @@ inc_dir=${build_dir%%/}/include
 lib_dir=${build_dir%%/}/lib
 pkg_dir=${lib_dir%%/}/pkgconfig
 PKG_CONFIG_PATH=${pkg_dir}:$PKG_CONFIG_PATH
-export CXXFLAGS=-std=c++11
-# # configure and install ThirdParty
-# mkdir -p ThirdParty/Blas
-# cd ThirdParty/Blas
-# ../../../ThirdParty/Blas/configure --prefix=$build_dir
-# make -j 10 install
-# cd ..
-# mkdir Lapack
-# cd Lapack
-# ../../../ThirdParty/Lapack/configure --prefix=$build_dir
-# make -j 10 install
-# cd ..
+export CXXFLAGS="-std=c++11 -g"
+# configure and install ThirdParty
+mkdir -p ThirdParty/Blas
+cd ThirdParty/Blas
+../../../ThirdParty/Blas/configure --prefix=$build_dir
+make -j 10 install
+cd ..
+mkdir Lapack
+cd Lapack
+../../../ThirdParty/Lapack/configure --prefix=$build_dir
+make -j 10 install
+cd ../..
 # mkdir Glpk
 # cd Glpk
 # ../../../ThirdParty/Glpk/configure --prefix=$build_dir
--- get_dependencies.sh
+++ get_dependencies.sh
@@ -2,10 +2,12 @@
 
 # get blas
 svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Blas/stable/1.4 ThirdParty/Blas
+cd ThirdParty/Blas && ./get.Blas && cd ../..
 # get lapack
 svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Lapack/stable/1.5 ThirdParty/Lapack
+cd ThirdParty/Blas && ./get.Lapack && cd ../..
 # coin glpk
-svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Glpk/stable/1.10 ThirdParty/Glpk
+# svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Glpk/stable/1.10 ThirdParty/Glpk
 # get coin-or coinutils
 svn co https://projects.coin-or.org/svn/CoinUtils/stable/2.10/CoinUtils
 # get coin-or osi
